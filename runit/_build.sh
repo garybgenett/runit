2f87c7db2f11a8520b4c422ff2e5fd41bf6fea4a
Gary B. Genett <me@garybgenett.net>
2009-11-16 15:10:27 -0800

    Renamed "maintx" runlevel to "maint_x" in "runit/_build.sh".

--- runit/_build.sh
+++ runit/_build.sh
@@ -7,7 +7,7 @@ rm -frv ./services/*/run
 rm -frv ./services/*/log/{config,run}
 
 mkdir -pv ./{log,runsvdir,services,watch}
-mkdir -pv ./runsvdir/{media,default,server,station,maint,maintx}
+mkdir -pv ./runsvdir/{media,default,server,station,maint,maint_x}
 
 for SVC in $(ls ./_config); do
 	mkdir -pv ./services/${SVC}
@@ -31,7 +31,7 @@ for SVC in $(ls ./_config); do
 	ln -fsv ../../services/${SVC}	./runsvdir/server/${SVC}
 	ln -fsv ../../services/${SVC}	./runsvdir/station/${SVC}
 	ln -fsv ../../services/${SVC}	./runsvdir/maint/${SVC}
-	ln -fsv ../../services/${SVC}	./runsvdir/maintx/${SVC}
+	ln -fsv ../../services/${SVC}	./runsvdir/maint_x/${SVC}
 done
 
 rm -frv ./runsvdir/*/{htc,hts,nxproxyc,nxproxys,xdm}
@@ -41,9 +41,9 @@ rm -frv ./runsvdir/default/{pavucontrol,projectm,pulseaudio}
 rm -frv  ./runsvdir/server/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,xvfb}
 rm -frv ./runsvdir/station/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,xvfb,_sync,dhcpd,dovecot,tftpd,thttpd,proftpd,named}
 rm -frv   ./runsvdir/maint/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,xvfb,_sync,dhcpd,dovecot,tftpd,thttpd,proftpd}
-rm -frv  ./runsvdir/maintx/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,xvfb,_sync,dhcpd,dovecot,tftpd,thttpd,proftpd}
+rm -frv ./runsvdir/maint_x/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,xvfb,_sync,dhcpd,dovecot,tftpd,thttpd,proftpd}
 
-rm -frv ./runsvdir/{media,default,server,maint,maintx}/{autossh,dhcpcd}
+rm -frv ./runsvdir/{media,default,server,maint,maint_x}/{autossh,dhcpcd}
 rm -frv ./runsvdir/{server,maint}/{xorg,xsession}
 
 exit 0
