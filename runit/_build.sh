c45c916d05a624251aaf45e6c9d0df0865df3fe3
Gary B. Genett <me@garybgenett.net>
2009-11-17 20:52:29 -0800

    Added "xvfb" to "server_r" runlevel in "runit/_build.sh".
    	* Also sorted "server_r" above "server"
    	* Included "xvfb" with "xorg, xsession"

--- runit/_build.sh
+++ runit/_build.sh
@@ -7,7 +7,7 @@ rm -frv ./services/*/run
 rm -frv ./services/*/log/{config,run}
 
 mkdir -pv ./{log,runsvdir,services,watch}
-mkdir -pv ./runsvdir/{media,default,server,server_r,maint,maint_x,station}
+mkdir -pv ./runsvdir/{media,default,server_r,server,maint,maint_x,station}
 
 for SVC in $(ls ./_config); do
 	mkdir -pv ./services/${SVC}
@@ -28,8 +28,8 @@ for SVC in $(ls ./_config); do
 	ln -fsv ../../_config/${SVC}	./services/${SVC}/run
 	ln -fsv ../../services/${SVC}	./runsvdir/media/${SVC}
 	ln -fsv ../../services/${SVC}	./runsvdir/default/${SVC}
-	ln -fsv ../../services/${SVC}	./runsvdir/server/${SVC}
 	ln -fsv ../../services/${SVC}	./runsvdir/server_r/${SVC}
+	ln -fsv ../../services/${SVC}	./runsvdir/server/${SVC}
 	ln -fsv ../../services/${SVC}	./runsvdir/maint/${SVC}
 	ln -fsv ../../services/${SVC}	./runsvdir/maint_x/${SVC}
 	ln -fsv ../../services/${SVC}	./runsvdir/station/${SVC}
@@ -39,23 +39,24 @@ rm -frv        ./runsvdir/*/{htc,hts,nxproxyc,nxproxys,xdm}
 
 rm -frv    ./runsvdir/media/{NULL,NULL}
 rm -frv  ./runsvdir/default/{pavucontrol,projectm,pulseaudio}
-rm -frv  ./runsvdir/server*/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,xvfb}
-rm -frv   ./runsvdir/maint*/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,xvfb,_sync,dhcpd,dovecot,tftpd,thttpd,proftpd}
-rm -frv  ./runsvdir/station/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,xvfb,_sync,dhcpd,dovecot,tftpd,thttpd,proftpd,named}
+rm -frv  ./runsvdir/server*/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd}
+rm -frv   ./runsvdir/maint*/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,_sync,dhcpd,dovecot,tftpd,thttpd,proftpd}
+rm -frv  ./runsvdir/station/{pavucontrol,projectm,pulseaudio,cupsd,dbus,hald,smbd,_sync,dhcpd,dovecot,tftpd,thttpd,proftpd,named}
 
 rm -frv    ./runsvdir/media/{autossh,dhcpcd}
 rm -frv  ./runsvdir/default/{autossh,dhcpcd}
-rm -frv   ./runsvdir/server/{autossh,dhcpcd}
 rm -frv ./runsvdir/server_r/{NULL,dhcpcd}
+rm -frv   ./runsvdir/server/{autossh,dhcpcd}
 rm -frv   ./runsvdir/maint*/{autossh,dhcpcd}
 rm -frv  ./runsvdir/station/{NULL,NULL}
 
 rm -frv    ./runsvdir/media/{NULL,NULL}
 rm -frv  ./runsvdir/default/{NULL,NULL}
-rm -frv  ./runsvdir/server*/{xorg,xsession}
-rm -frv    ./runsvdir/maint/{xorg,xsession}
-rm -frv  ./runsvdir/maint_x/{NULL,NULL}
-rm -frv  ./runsvdir/station/{NULL,NULL}
+rm -frv ./runsvdir/server_r/{xorg,xsession}
+rm -frv   ./runsvdir/server/{xorg,xsession,xvfb}
+rm -frv    ./runsvdir/maint/{xorg,xsession,xvfb}
+rm -frv  ./runsvdir/maint_x/{NULL,xvfb}
+rm -frv  ./runsvdir/station/{NULL,xvfb}
 
 exit 0
 # end of file
