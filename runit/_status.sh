a64697a5aca6bdec73372711de1312ae7cef4401
Gary B. Genett <me@garybgenett.net>
2026-01-31 11:38:34 -0800

    Updated "_status.sh" script in "runit".
    	* First edit!  Goodness, almost 12 years...

--- runit/_status.sh
+++ runit/_status.sh
@@ -3,24 +3,26 @@ source ${HOME}/.bashrc
 ################################################################################
 
 declare SV_CMD="$(which sv)"
+declare SVC=
 
 ################################################################################
 
-declare SVC=
 for SVC in $(ls $(dirname ${0})/_config); do
-	sv status ${SVC}
+	${SV_CMD} status ${SVC}
 done
-
-########################################
-
 echo
+for SVC in $(psg "runsv[ ]" | awk '{print $12;}' | sort); do
+	${SV_CMD} status ${SVC}
+done
 
 ########################################
 
-declare SVC=
-for SVC in $(psg "runsv[ ]" | awk '{print $12;}' | sort); do
-	sv status ${SVC}
-done
+if [[ -n ${1} ]]; then
+	declare SVC="${1}" && shift
+	echo
+	${SV_CMD} restart ${SVC}
+	tail -f /.runit/log/${SVC}
+fi
 
 exit 0
 ################################################################################
